84634a93641fcd2a7818ab7032c1d6670ed0a223
afroozrahmati <afr.rahmati@gmail.com>
2021-11-10 16:40:27 -0500

    split excel files for each client

--- clients_data_generation.py
+++ clients_data_generation.py
@@ -1,7 +1,9 @@
 import os
 import pandas as pd
 
-df_normal = pd.read_csv('data/normal.csv')
+os.chdir(r'D:\UW\RA\Intrusion_Detection')
+
+df_normal = pd.read_csv('./data/normal.csv')
 df_abnormal = pd.read_csv('data/abnormal.csv')
 number_clinets=10
 
@@ -9,7 +11,7 @@ normal_data_rows_per_client = len(df_normal)//number_clinets
 abnormal_data_rows_per_client = len(df_abnormal)//number_clinets
 
 def split(filehandler, delimiter=',', row_limit=1000,
-          output_name_template='output_%s.csv', output_path='.', keep_headers=True):
+          output_name_template='output_%s.csv', output_path='./data/', keep_headers=True):
     import csv
     reader = csv.reader(filehandler, delimiter=delimiter)
     current_piece = 1
@@ -17,10 +19,11 @@ def split(filehandler, delimiter=',', row_limit=1000,
         output_path,
         output_name_template % current_piece
     )
-    current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    #current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+    current_out_writer = csv.writer(open(current_out_path, 'w', newline=''), delimiter=delimiter)
     current_limit = row_limit
     if keep_headers:
-        headers = reader.next()
+        headers = next(reader)
         current_out_writer.writerow(headers)
     for i, row in enumerate(reader):
         if i + 1 > current_limit:
@@ -30,10 +33,11 @@ def split(filehandler, delimiter=',', row_limit=1000,
                 output_path,
                 output_name_template % current_piece
             )
-            current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            #current_out_writer = csv.writer(open(current_out_path, 'w'), delimiter=delimiter)
+            current_out_writer = csv.writer(open(current_out_path, 'w', newline=''), delimiter=delimiter)
             if keep_headers:
                 current_out_writer.writerow(headers)
         current_out_writer.writerow(row)
 
-split(open('data/normal.csv', 'r'),row_limit=normal_data_rows_per_client);
-split(open('data/abnormal.csv', 'r'),row_limit=abnormal_data_rows_per_client);
+split(open('data/normal.csv', 'r'), row_limit=normal_data_rows_per_client,output_name_template='normal_%s.csv');
+split(open('data/abnormal.csv', 'r'),row_limit=abnormal_data_rows_per_client,output_name_template='abnormal_%s.csv');
