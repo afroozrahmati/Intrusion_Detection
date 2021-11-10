9d9e38fbd3051a42ca53ebf218995e3599fd4619
afroozrahmati <afr.rahmati@gmail.com>
2021-11-10 16:56:56 -0500

    Config Server file

--- client.py
+++ client.py
@@ -1,2 +1,37 @@
 import sys
-sys.argv[0]
+#file name as an input argument for each client
+#sys.argv[0]
+import os
+import ssl
+import flwr as fl
+import tensorflow as tf
+
+# Make TensorFlow log less verbose
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"
+#tensorflow version shoudl be 2.4.0
+
+if __name__ == "__main__":
+    # Load CIFAR-10 dataset
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+    # Load and compile Keras model
+    model = tf.keras.applications.MobileNetV2((32, 32, 3), classes=10, weights=None)
+    model.compile("adam", "sparse_categorical_crossentropy", metrics=["accuracy"])
+
+    # Define Flower client
+    class CifarClient(fl.client.NumPyClient):
+        def get_parameters(self):  # type: ignore
+            return model.get_weights()
+
+        def fit(self, parameters, config):  # type: ignore
+            model.set_weights(parameters)
+            model.fit(x_train, y_train, epochs=1, batch_size=32)
+            return model.get_weights(), len(x_train), {}
+
+        def evaluate(self, parameters, config):  # type: ignore
+            model.set_weights(parameters)
+            loss, accuracy = model.evaluate(x_test, y_test)
+            return loss, len(x_test), {"accuracy": accuracy}
+
+    # Start Flower client
+    fl.client.start_numpy_client("172.29.128.1:8080", client=CifarClient())
