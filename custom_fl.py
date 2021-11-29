e8234d28940a2932681b41be19a285fdb2bf59c1
afroozrahmati <afr.rahmati@gmail.com>
2021-11-29 11:07:55 -0500

    Add citation

--- custom_fl.py
+++ custom_fl.py
@@ -1,4 +1,5 @@
-
+#reference to this intresting artile to create a simple custom FL model
+#https://towardsdatascience.com/federated-learning-a-step-by-step-implementation-in-tensorflow-aac568283399
 import numpy as np
 import random
 import os
@@ -35,6 +36,51 @@ def load_processed_data(file_path_normal,file_path_abnormal):
     return x_train,y_train,x_test, y_test
 
 
+def create_clients(image_list, label_list, num_clients=10, initial='clients'):
+    ''' return: a dictionary with keys clients' names and value as
+                data shards - tuple of images and label lists.
+        args:
+            image_list: a list of numpy arrays of training images
+            label_list:a list of binarized labels for each image
+            num_client: number of fedrated members (clients)
+            initials: the clients'name prefix, e.g, clients_1
+
+    '''
+
+    # create a list of client names
+    client_names = ['{}_{}'.format(initial, i + 1) for i in range(num_clients)]
+
+    # randomize the data
+    data = list(zip(image_list, label_list))
+    random.shuffle(data)
+
+    # shard data and place at each client
+    size = len(data) // num_clients
+    shards = [data[i:i + size] for i in range(0, size * num_clients, size)]
+
+    # number of clients must equal number of shards
+    assert (len(shards) == len(client_names))
+
+    return {client_names[i]: shards[i] for i in range(len(client_names))}
+
+
+def batch_data(data_shard, bs=32):
+    '''Takes in a clients data shard and create a tfds object off it
+    args:
+        shard: a data, label constituting a client's data shard
+        bs:batch size
+    return:
+        tfds object'''
+    #seperate shard into data and labels lists
+    data, label = zip(*data_shard)
+    dataset = tf.data.Dataset.from_tensor_slices((list(data), list(label)))
+    return dataset.shuffle(len(label)).batch(bs)
+
+
 file_path_normal = 'D:\\UW\\RA\\Intrusion_Detection\\data\\normal.csv'  # sys.argv[1] #    #+ sys.argv[0]
 file_path_abnormal = 'D:\\UW\\RA\\Intrusion_Detection\\data\\abnormal.csv'  # sys.argv[2] #  #+ sys.argv[1]
 x_train, y_train, x_test, y_test = load_processed_data(file_path_normal, file_path_abnormal)  # args.partition)
+
+#create clients
+clients = create_clients(x_train, y_train, num_clients=10, initial='client')
+
