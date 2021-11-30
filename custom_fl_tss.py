cfd3fc5252f5e0cc549a4fddaad2d1e66091962b
afroozrahmati <afr.rahmati@gmail.com>
2021-11-30 14:42:29 -0500

    remove warning messages in custom_fl_tss.py

--- custom_fl_tss.py
+++ custom_fl_tss.py
@@ -13,6 +13,8 @@ from ClusteringLayer import *
 import sklearn.metrics.pairwise as smp
 import math
 import torch
+import warnings
+warnings.filterwarnings("ignore")
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
